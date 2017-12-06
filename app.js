f3496efffb31512a11a69e8704786737c1b3b322
HenryVen <heve9547@colorado.edu>
2017-12-06 21:33:13 +1300

    working on snake game

--- app.js
+++ app.js
@@ -1,18 +1,47 @@
-console.log("HelloWorld")
+
 // Snake Game 
+// Math.random""   random number 0-1
+// Math.round()   round to whole
 var counter = 0;
 var delay = 10;
 var boxWidth = 500;
 var boxHeight = 300;
+var foodCord = Math.round(Math.random()*boxWidth);
+
+/*Fixing food spawning on the sides
+if (foodCord <10 or >(boxWidth-10)) {
+	foodCord = Math.round(Math.random()*boxWidth);
+} 
+*/
+
+console.log(foodCord)
 function gameLoop() {
 	counter = counter + 1;
-	console.log("counter=", counter)
 	d3.select(".snake")
 		.attr("x", counter);
+
+	d3.select(".food")
+		.attr("x", foodCord);
+
+	if (counter === foodCord) {
+		d3.select(".food")
+			foodCord = Math.round(Math.random()*boxWidth);
+		console.log("nomnom")
+
+		d3.select("#snakeBox")
+		.append("rect")
+		.attr("class", "body")
+		.attr("width", 10)
+		.attr("height", 10)
+		.attr("fill", "green")
+		.attr("x", counter - 1);
+	}
+		
 //is snake outside box lose man
 	if (counter > boxWidth) {
 		console.log("Snakes outta the box!")
 		return
+	
 	}
 
 	setTimeout(function() {
@@ -33,6 +62,13 @@ function startGame(){
 		.attr("width", 10)
 		.attr("height", 10)
 		.attr("fill", "green")
+
+	d3.select("#snakeBox")
+		.append("rect")
+		.attr("class", "food")
+		.attr("width", 10)
+		.attr("height", 10)
+		.attr("fill", "rgb(250,200,200)")
 
 	gameLoop();
 }
